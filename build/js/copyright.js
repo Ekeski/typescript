c5119acbb0c56a7d5afb16167c8023c0b6da175a
Ekeski <ekemewomaogbofavour@gmail.com>
2026-01-29 22:15:28 +0000

    Lesson tutorial

---
+++ build/js/copyright.js
@@ -0,0 +1,17 @@
+// Original JS code
+// const year = document.getElementById("year") 
+// const thisYear = new Date().getFullYear()  
+// year.setAttribute("datetime", thisYear) 
+// year.textContent = thisYear
+export {};
+// 1st variation:
+// let year: HTMLElement | null 
+// year = document.getElementById("year") 
+// let thisYear: string  
+// thisYear = new Date().getFullYear().toString() 
+// if (year) {
+//     year.setAttribute("datetime", thisYear) 
+//     year.textContent = thisYear
+// } 
+// 2nd variation:-
+//# sourceMappingURL=copyright.js.map
